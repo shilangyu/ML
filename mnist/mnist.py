72ab57333362b356a28d7312a46ee5d73eca3b30
shilangyu <xmarcinmarcin@gmail.com>
2019-05-05 16:17:42 +0200

    renamed image to pixel and added training

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,8 +1,10 @@
 import gzip
-import sys
+import argparse
+import numpy as np
+from NN import NN
 
 
-def load_dataset(image_data_path, label_data_path, to, amount):
+def load_dataset(image_data_path, label_data_path, to_pixels, to_labels, amount):
     f = gzip.open(image_data_path, "rb")
     l = gzip.open(label_data_path, "rb")
 
@@ -10,19 +12,47 @@ def load_dataset(image_data_path, label_data_path, to, amount):
     l.read(8)
 
     for i in range(amount):
-        to.append({'label': ord(l.read(1))})
+        to_labels.append(ord(l.read(1)))
         image = []
         for j in range(28*28):
             image.append(ord(f.read(1)))
-        to[-1]['pixels'] = image
+        to_pixels.append(image)
 
     f.close()
     l.close()
 
 
-train_data = []
-test_data = []
+def map_labels(position):
+    empty = np.array(range(10))*0
+    empty[position] = 1
+    return empty
 
-load_dataset("train_images.gz", "train_labels.gz", train_data, 60000)
-load_dataset("test_images.gz", "test_labels.gz", test_data, 10000)
-print('done loading data')
+
+def map_pixels(pixel_array):
+    return list(map(lambda x: x/255, pixel_array))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Creates a NN for the classical mnist dataset (without a convolutional layer)')
+    parser.add_argument('train_amount', type=int,
+                        help='integer representing the amount of training sessions')
+    args = parser.parse_args()
+
+    train_pixels = []
+    train_labels = []
+    test_pixels = []
+    test_labels = []
+
+    load_dataset("train_pixels.gz", "train_labels.gz",
+                 train_pixels, train_labels, 5000)
+    load_dataset("test_pixels.gz", "test_labels.gz",
+                 test_pixels, test_labels, 5000)
+
+    mnistnn = NN(28*28, 16, 10, 2,
+                 lambda x: 1 / (1 + np.exp(-x)), lambda y: y * (1-y), 0.1)
+
+    mapped_train_pixels = list(map(map_pixels, train_pixels))
+    mapped_train_labels = list(map(map_labels, train_labels))
+
+    mnistnn.train(mapped_train_pixels, mapped_train_labels, args.train_amount)
